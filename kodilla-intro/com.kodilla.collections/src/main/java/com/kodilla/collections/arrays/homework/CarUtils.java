d56500e240ce39331e89931a4d8df27d4afcc7db
Patross <aleksandrawalczak0306@gmail.com>
2023-01-21 14:31:35 +0000

    4.2 - zadanie

--- kodilla-intro/com.kodilla.collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
+++ kodilla-intro/com.kodilla.collections/src/main/java/com/kodilla/collections/arrays/homework/CarUtils.java
@@ -1,9 +1,27 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Audi;
+import com.kodilla.collections.interfaces.homework.Bmw;
 import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ferrari;
+
 
 public class CarUtils {
-    public static void describeCar(Car car){
 
+    public static void describeCar(Car car) {
+        System.out.println("-----------------------------");
+        System.out.println("Car name: " + getCarName(car));
+        System.out.println("Car speed: " + car.getSpeed());
+    }
+
+    private static String getCarName(Car car) {
+        if (car instanceof Ferrari)
+            return "Ferrari";
+        else if (car instanceof Bmw)
+            return "Bmw";
+        else if (car instanceof Audi)
+            return "Audi";
+        else
+            return "Unknown car name";
     }
 }
--- kodilla-intro/com.kodilla.collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
+++ kodilla-intro/com.kodilla.collections/src/main/java/com/kodilla/collections/arrays/homework/CarsApplication.java
@@ -1,7 +1,40 @@
 package com.kodilla.collections.arrays.homework;
 
+import com.kodilla.collections.interfaces.homework.Audi;
+import com.kodilla.collections.interfaces.homework.Bmw;
 import com.kodilla.collections.interfaces.homework.Car;
+import com.kodilla.collections.interfaces.homework.Ferrari;
+
+import java.util.Random;
 
 public class CarsApplication {
+    public static Car drawCar() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(3);
+        Car car;
+        if (randomNumber == 0) {
+            car = new Ferrari();
+        } else if (randomNumber == 1) {
+            car = new Bmw();
+        } else {
+            car = new Audi();
+        }
+        int randomSpeed = random.nextInt(10) + 1;
+        for (int i = 0; i < randomSpeed; i++) {
+            car.increaseSpeed();
+        }
+        return car;
+    }
 
+    public static void main(String[] args) {
+        Random random = new Random();
+        int randomSize = random.nextInt(15) + 1;
+        Car[] cars = new Car[randomSize];
+        for (int i = 0; i < cars.length; i++) {
+            cars[i] = drawCar();
+        }
+        for (Car car : cars) {
+            CarUtils.describeCar(car);
+        }
+    }
 }
